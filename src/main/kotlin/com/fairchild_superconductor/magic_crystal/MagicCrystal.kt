43a2a5e728999ed2ba7bb7212b5862c6e2a4f4f3
longfangsong <longfangsong@icloud.com>
2021-07-13 14:28:20 +0800

    Initial commit

---
+++ src/main/kotlin/com/fairchild_superconductor/magic_crystal/MagicCrystal.kt
@@ -0,0 +1,11 @@
+package com.fairchild_superconductor.magic_crystal
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("UNUSED")
+object MagicCrystal : ModInitializer {
+    private const val MOD_ID = "magic_crystal"
+    override fun onInitialize() {
+        println("Magic Crystal mod has been initialized.")
+    }
+}
