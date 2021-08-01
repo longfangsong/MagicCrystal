cb3c21b034e801a145643d9a84a880aee3fd5190
longfangsong <longfangsong@icloud.com>
2021-08-02 01:42:38 +0800

    make the sampling growing!

--- src/main/kotlin/com/fairchild_superconductor/magic_crystal/MagicCrystal.kt
+++ src/main/kotlin/com/fairchild_superconductor/magic_crystal/MagicCrystal.kt
@@ -3,31 +3,45 @@ package com.fairchild_superconductor.magic_crystal
 import com.fairchild_superconductor.magic_crystal.computer.ComputerBlock
 import com.fairchild_superconductor.magic_crystal.computer.ComputerEntity
 import com.fairchild_superconductor.magic_crystal.rubber_tree.RubberLogBlock
+import com.fairchild_superconductor.magic_crystal.rubber_tree.RubberSaplingBlock
+import com.fairchild_superconductor.magic_crystal.rubber_tree.RubberSaplingGenerator
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.`object`.builder.v1.block.entity.FabricBlockEntityTypeBuilder
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors
-import net.minecraft.block.Block
-import net.minecraft.block.Material
+import net.minecraft.block.*
 import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
 import net.minecraft.util.Identifier
+import net.minecraft.util.collection.DataPool
+import net.minecraft.util.math.intprovider.ConstantIntProvider
 import net.minecraft.util.registry.BuiltinRegistries
 import net.minecraft.util.registry.Registry
 import net.minecraft.util.registry.RegistryKey
+import net.minecraft.world.biome.Biome
+import net.minecraft.world.biome.BiomeKeys
 import net.minecraft.world.gen.GenerationStep
 import net.minecraft.world.gen.YOffset
 import net.minecraft.world.gen.decorator.RangeDecoratorConfig
 import net.minecraft.world.gen.feature.ConfiguredFeature
 import net.minecraft.world.gen.feature.Feature
 import net.minecraft.world.gen.feature.OreFeatureConfig
+import net.minecraft.world.gen.feature.TreeFeatureConfig
+import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize
+import net.minecraft.world.gen.foliage.BlobFoliagePlacer
 import net.minecraft.world.gen.heightprovider.UniformHeightProvider
+import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider
+import net.minecraft.world.gen.stateprovider.WeightedBlockStateProvider
+import net.minecraft.world.gen.trunk.StraightTrunkPlacer
+import java.util.*
 
 
 var COMPUTER_ENTITY: BlockEntityType<ComputerEntity>? = null
+
+var RUBBER_TREE_CONFIGURE: ConfiguredFeature<TreeFeatureConfig, *>? = null
 
 @Suppress("UNUSED")
 object MagicCrystal : ModInitializer {
@@ -36,6 +50,7 @@ object MagicCrystal : ModInitializer {
     val TIN_ORE_BLOCK: Block = Block(FabricBlockSettings.of(Material.STONE).hardness(1.5f))
     val RUBBER_LEAVES_BLOCK: Block = Block(FabricBlockSettings.of(Material.LEAVES).hardness(1.0f))
     val RUBBER_LOG_BLOCK = RubberLogBlock()
+    val RUBBER_SAPLING_BLOCK = RubberSaplingBlock(RubberSaplingGenerator(), FabricBlockSettings.of(Material.LEAVES))
     var COMPUTER_ITEM: BlockItem? = null
     override fun onInitialize() {
         val TIN_ORE_OVERWORLD: ConfiguredFeature<*, *> = Feature.ORE
@@ -116,6 +131,17 @@ object MagicCrystal : ModInitializer {
             BlockItem(RUBBER_LOG_BLOCK, Item.Settings().group(ItemGroup.MISC))
         )
 
+        Registry.register(
+            Registry.BLOCK,
+            Identifier(MOD_ID, "rubber_sapling"),
+            RUBBER_SAPLING_BLOCK
+        )
+        Registry.register(
+            Registry.ITEM,
+            Identifier(MOD_ID, "rubber_sapling"),
+            BlockItem(RUBBER_SAPLING_BLOCK, Item.Settings().group(ItemGroup.MISC))
+        )
+
         val tinOreOverworld = RegistryKey.of(
             Registry.CONFIGURED_FEATURE_KEY,
             Identifier(MOD_ID, "tin_ore")
@@ -125,6 +151,34 @@ object MagicCrystal : ModInitializer {
             BiomeSelectors.foundInOverworld(),
             GenerationStep.Feature.UNDERGROUND_ORES,
             tinOreOverworld
+        )
+
+        val rubberTreeKey = RegistryKey.of(
+            Registry.CONFIGURED_FEATURE_KEY,
+            Identifier(MOD_ID, "rubber_tree")
+        )
+        RUBBER_TREE_CONFIGURE = Feature.TREE.configure(
+            TreeFeatureConfig.Builder(
+                SimpleBlockStateProvider(RUBBER_LOG_BLOCK.defaultState),
+                StraightTrunkPlacer(5, 3, 0),
+                SimpleBlockStateProvider(RUBBER_LEAVES_BLOCK.defaultState),
+                SimpleBlockStateProvider(RUBBER_SAPLING_BLOCK.defaultState),
+                BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 3),
+                TwoLayersFeatureSize(1, 0, 1)
+            ).build()
+        )
+
+        Registry.register(
+            BuiltinRegistries.CONFIGURED_FEATURE,
+            Identifier(MOD_ID, "rubber_tree"),
+            RUBBER_TREE_CONFIGURE
+        )
+        val forestBiomes: Collection<RegistryKey<Biome>> =
+            listOf(BiomeKeys.FOREST, BiomeKeys.WOODED_HILLS, BiomeKeys.FLOWER_FOREST)
+
+        BiomeModifications.addFeature(
+            BiomeSelectors.includeByKey(forestBiomes),
+            GenerationStep.Feature.VEGETAL_DECORATION, rubberTreeKey
         )
 
         println("Magic Crystal mod has been initialized.")
---
+++ src/main/kotlin/com/fairchild_superconductor/magic_crystal/rubber_tree/RubberSaplingBlock.kt
@@ -0,0 +1,7 @@
+package com.fairchild_superconductor.magic_crystal.rubber_tree
+
+import net.minecraft.block.SaplingBlock
+import net.minecraft.block.sapling.SaplingGenerator
+
+class RubberSaplingBlock(generator: SaplingGenerator?, settings: Settings?) : SaplingBlock(generator, settings) {
+}
